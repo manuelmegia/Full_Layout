bc5a237f395c4bc4ddf0128abbd7a3ed6ea082e3
ManueMegia <manmeg423@alu.centroafuera.es>
2023-02-06 09:55:09 +0100

    todo acabado

--- app/src/main/java/com/example/full_layout/MainActivity.kt
+++ app/src/main/java/com/example/full_layout/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.full_layout
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.full_layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.imageButton3.setOnClickListener{
+            val nuevaVista2 = Intent(this, Navigate::class.java)
+            startActivity(nuevaVista2)
+        }
+
+        binding.imageButton4.setOnClickListener{
+            Toast.makeText(this, "No se puede llamar, inserte número de teléfono", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.imageButton5.setOnClickListener{
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/"))
+            startActivity(i)
+        }
+
+        binding.imageButton6.setOnClickListener{
+            binding.imageButton6.setBackgroundColor(Color.BLACK)
+        }
+
+        binding.imageButton7.setOnClickListener{
+            val b = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/gonjim967"))
+            startActivity(b)
+        }
+
+        binding.imageButton8.setOnClickListener{
+            binding.imageButton8.setImageResource(R.drawable.youtube)
+        }
     }
 }
---
+++ app/src/main/java/com/example/full_layout/Navigate.kt
@@ -0,0 +1,11 @@
+package com.example.full_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Navigate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigate)
+    }
+}
